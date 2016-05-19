c3780a9333d79f81c8d5c63b131bf168aae0f034
Fake Name <fakename@gmail.com>
2016-05-18 20:22:55 -0400

    added verbs project

---
+++ js/verbs.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+		//console.log
+
+		var verbs = ['snowboarding', 'surfing', 'mountain-climbing'];
+
+
+
+
+		for( i = 0; i < 3; i++ ){
+   		console.log( i );
+   		alert("I always knew I'd be great at  "+ verbs[i]);
+   	}
+
+
+});
